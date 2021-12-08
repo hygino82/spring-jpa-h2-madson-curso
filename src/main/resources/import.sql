6b8a204471ae8cceb19419cb980486de5784f324
Adroaldo <hygino@msn.com>
2021-12-07 21:07:54 -0300

    Adicionado mais clientes ao banco de dados inicial

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
-INSERT INTO cliente(id, cpf, email, nome) VALUES (1, '1234578','gorete@gmail.com','Gorete');
+INSERT INTO cliente(id, cpf, email, nome) VALUES (1, '1234578','gorete@gmail.com','Gorete Medeiros');
+INSERT INTO cliente(id, cpf, email, nome) VALUES (2, '4950223','jupira@gmail.com','Jupira Alves');
+INSERT INTO cliente(id, cpf, email, nome) VALUES (3, '8964786','godofredo@gmail.com','Godofredo Nunes');
